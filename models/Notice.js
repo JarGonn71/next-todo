5a005297d2d8fbf25bf2525173917016da25e785
Kirill <kirill.nyuhtilin@gmail.com>
2021-10-16 14:21:12 +0300

    add color schema

--- models/Notice.js
+++ models/Notice.js
@@ -4,6 +4,7 @@ const schema = new Schema({
     title: {type: String, require: true},
     todo: [{}],
     timeInfo: {type: String, require: true},
+    color: {type: String, require: true},
 })
 
 module.exports = model('Notice', schema)
